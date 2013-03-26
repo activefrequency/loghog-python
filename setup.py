f24dbdd357cc22d8735ea034454553b4371ba854
Igor Partola <igor@igorpartola.com>
2013-03-26 16:32:14 -0400

    Typo fix: license name in setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache2 License',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: POSIX',
         'Programming Language :: Python',
